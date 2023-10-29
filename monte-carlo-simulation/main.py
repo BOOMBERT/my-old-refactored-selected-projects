51bfcb0da2718ed88e15a1b44aa11b1e521556c1
Norbert Domagala <norbert.domagala2005@gmail.com>
2023-10-29 18:41:30 +0100

    Refactored code

--- monte-carlo-simulation/main.py
+++ monte-carlo-simulation/main.py
@@ -1,101 +1,129 @@
 import turtle
+from typing import Tuple
 from random import randint
 from math import sqrt
 from time import sleep
+from tkinter import TclError
 
-#===WINDOW===
-wn = turtle.Screen()
-wn.setup(500,500)
-wn.bgcolor('black')
-wn.title('Number pi by method - Monte Carlo')
 
-#===START_OPTIONS===
-turtle.speed(0)
-turtle.ht()
-running = True
+class Colors:
+    BACKGROUND: str = "black"
+    MAIN: str = "white"
+    EFFECT: str = "blue"
+    CORRECT: str = "green"
+    INCORRECT: str = "red"
 
-#===TEMPLATE===
-def board():
-    def square():
-        turtle.color('blue')
-        for i in range(4):
-            turtle.forward(200)
+
+class MonteCarloBoard:
+    def __init__(self, square_color: str, circle_color: str, board_length: int) -> None:
+        self.square_color = square_color
+        self.circle_color = circle_color
+        self.board_length = board_length
+
+    def square(self) -> None:
+        turtle.color(self.square_color)
+        for _ in range(4):
+            turtle.forward(self.board_length)
             turtle.left(90)
-    square()
 
-    def circle():
-        turtle.color('white')
-        turtle.circle(100)
-    turtle.pu()
-    turtle.goto(100, 0)
-    turtle.pd()
-    circle()
+    def circle(self) -> None:
+        turtle.color(self.circle_color)
+        turtle.circle(self.board_length // 2)
 
-board()
-
-#===INFORMATION===
-def writing():
-    turtle.color('blue')
-    turtle.up()
-    turtle.goto(-225,175)
-    turtle.write('Number:', font=("normal", 15, "normal"))
-
-    turtle.goto(-225,125)
-    turtle.write('Green points:', font=("normal", 12, "normal"))
-
-    turtle.goto(-225,75)
-    turtle.write('Red points:', font=("normal", 12, "normal"))
-
-    turtle.goto(-225,25)
-    turtle.write('All points:', font=("normal", 12, "normal"))
-
-writing()
-
-#===MAIN_PROGRAM===
-def main():
-    n = m = 0
-    while running:
-        #===RANDOM===
-        x, y = randint(0,200), randint(0,200)
+    def draw(self) -> None:
+        self.square()
         turtle.pu()
-        turtle.goto(x, y)
+        turtle.setx(turtle.pos()[0] + self.board_length // 2)
+        turtle.pd()
+        self.circle()
+
+
+def draw_data(
+        data: Tuple,
+        font_size: int,
+        font_name: str = "normal",
+        font_type: str = "normal",
+        turtle_to_draw: turtle.Turtle = turtle,
+) -> None:
+    start_y_position = turtle_to_draw.pos()[1]
+    for index, data in enumerate(data):
+        turtle_to_draw.sety(start_y_position - index * (font_size * 4))
+        turtle_to_draw.write(data, font=(font_name, font_size, font_type))
+
+
+def simulation(board_start_position: Tuple[float, float], board_length: int, font_size: int) -> None:
+    DOT_SIZE = 2
+    dots_in_circle = all_dots = 0
+    half_of_board_length = board_length // 2
+
+    turtle_to_changing_data = turtle.Turtle()
+    turtle_to_changing_data.ht()
+    turtle_to_changing_data.speed(0)
+    turtle_to_changing_data.color(Colors.MAIN)
+    turtle_to_changing_data.pu()
+    turtle_to_changing_data.setx(board_start_position[0] - 200)
+
+    while True:
+        point_x_position = randint(0, board_length)
+        point_y_position = randint(0, board_length)
+        turtle.pu()
+        turtle.goto(point_x_position + board_start_position[0], point_y_position + board_start_position[1])
         turtle.pd()
 
-        #===CALCULATIONS===
-        m += 1
-        if (sqrt((x-100)**2 + (y-100)**2)) <= 100:
-            turtle.dot(2, 'green')
-            n += 1
+        all_dots += 1
+        if ((
+                sqrt(
+                (point_x_position - half_of_board_length) ** 2 +
+                (point_y_position - half_of_board_length) ** 2
+                )) <= half_of_board_length):
+            turtle.dot(DOT_SIZE, Colors.CORRECT)
+            dots_in_circle += 1
         else:
-            turtle.dot(2, 'red')
-        number_pi = (n * 4) / m
+            turtle.dot(DOT_SIZE, Colors.INCORRECT)
 
-        # ===HELPFUL_TURTLE===
-        turtle2 = turtle.Turtle()
+        number_pi = (dots_in_circle * 4) / all_dots
 
-        # ===DISPLAYING_ALL_DATA===
-        def display():
-            #===HELPFUL_TURTLE_SETTINGS===
-            turtle2.ht()
-            turtle2.speed(0)
-            turtle2.color('white')
-            turtle2.up()
+        turtle_to_changing_data.sety(board_start_position[1] + board_length - font_size * 3)
 
-            turtle2.goto(-225, 150)
-            turtle2.write(number_pi, font=("normal", 15, "normal"))
+        data_to_draw = (number_pi, dots_in_circle, all_dots - dots_in_circle, all_dots)
+        draw_data(data_to_draw, font_size, turtle_to_draw=turtle_to_changing_data)
 
-            turtle2.goto(-225, 100)
-            turtle2.write(n, font=("normal", 15, "normal"))
-
-            turtle2.goto(-225, 50)
-            turtle2.write(m - n, font=("normal", 15, "normal"))
-
-            turtle2.goto(-225, 0)
-            turtle2.write(m, font=("normal", 15, "normal"))
-
-        display()
         sleep(0.2)
-        turtle2.clear()
+        turtle_to_changing_data.clear()
 
-try: main()
-except turtle.Terminator: running = False
+
+def main() -> None:
+    WIDTH = 500
+    HEIGHT = 500
+
+    window = turtle.Screen()
+    window.setup(WIDTH, HEIGHT)
+    window.bgcolor(Colors.BACKGROUND)
+    window.title("Number pi by method - Monte Carlo")
+
+    turtle.speed(0)
+    turtle.ht()
+
+    BOARD_LENGTH = 200
+    FONT_SIZE = 15
+
+    turtle.sety(-BOARD_LENGTH // 2)
+    board_start_position = turtle.pos()
+
+    board = MonteCarloBoard(Colors.EFFECT, Colors.MAIN, BOARD_LENGTH)
+    board.draw()
+
+    DATA_INFO_TO_DRAW = ("Number:", "Green points:", "Red points:", "All points:")
+    turtle.color(Colors.EFFECT)
+    turtle.pu()
+    turtle.goto(board_start_position[0] - 200, board_start_position[1] + BOARD_LENGTH - FONT_SIZE)
+    draw_data(DATA_INFO_TO_DRAW, FONT_SIZE)
+
+    simulation(board_start_position, BOARD_LENGTH, FONT_SIZE)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except (turtle.Terminator, TclError):
+        pass
